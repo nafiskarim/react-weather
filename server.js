74529afa8ea7570457876c9b841e62585a44a4ba
nafiskarim <sahil.abrar@gmail.com>
2016-11-23 21:45:25 +0100

    server settings changed for local and live

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-if (PORT != 3000) {
-  // redirect https to http
-  app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-      next();
-    }else {
-      res.redirect('http://' + req.hostname + req.url);
-    }
-  });
-}
+// redirect https to http
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  }else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
