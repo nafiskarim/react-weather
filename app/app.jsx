b01160268739713694f745b92c24b29d2d2f3c5d
nafiskarim <sahil.abrar@gmail.com>
2016-11-23 23:08:42 +0100

    foundation, jquery, script loader added

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// load foundation. and style loader and css loader to work with css files
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory} >
     <Route path='/' component={Main} >
